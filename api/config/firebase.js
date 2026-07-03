f1e6ea2ad8f59ffd8505a197170db6cd57f54837
Jeevesh Chaurasiya <jeeveshchaurasiya@gmail.com>
2026-07-03 12:02:38 +0530

    setup supabase and firebase config

---
+++ api/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp, cert, getApps } from 'firebase-admin/app'
+
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+    })
+  })
+}
+
